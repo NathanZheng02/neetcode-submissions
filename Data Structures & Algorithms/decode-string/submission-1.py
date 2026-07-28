80644881a106b518523472fdc35a18883553770d
Nathan Zheng <NathanZheng50@gmail.com>
2026-07-27 22:52:54 -0500

    Add: decode-string - submission-1

---
+++ Data Structures & Algorithms/decode-string/submission-1.py
@@ -0,0 +1,68 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        # [] indicates a call to recurse
+        # Key is that s = 1[s], so we can start with call
+
+        # If we hit [], we need to recurse
+        # Recursion returns unraveled string - Ex: 2[a3[b]] would
+        # give us 2[abbb] -> abbbabbb. This works because we
+        # return from the top of the recursion call stack first
+        
+        # If string has char, just add to output string
+        # If contains number, read number until "[" and store number
+        # Call recurse with string what is in between "[]" - determine
+        # whenever a "]" is seen. Return output string when "]"
+
+        # Parameters:
+            # String to parse: Ex: a3[b]
+            # Index to store position
+        # Returns:
+            # Output string
+
+
+        # def recurse(string, i):
+        #     out = ""
+        #     val = 0
+        #     while i < len(string):
+        #         c = string[i]
+        #         if c.isdigit():
+        #             val = val * 10 + int(c)
+        #             i += 1
+        #         elif c == "[":
+        #             res, idx = recurse(string, i + 1)
+        #             out += res * val
+        #             i = idx
+        #             val = 0
+        #         elif c == "]":
+        #             return out, i + 1
+        #         else:
+        #             out += c
+        #             i += 1
+        #     return out, i
+            
+        # return recurse(s, 0)[0]
+
+
+        # Method without Recursion
+        # To convert the recursive method to an interative one,
+        # we need to save the current string that we are building
+        # in a stack and resolve inner most content
+
+        stack = []
+        curr = ""
+        curr_amt = 0
+
+        for c in s:
+            if c.isdigit():
+                curr_amt = curr_amt * 10 + int(c)
+            elif c == "[":
+                stack.append((curr, curr_amt))
+                curr = ""
+                curr_amt = 0
+            elif c == "]":
+                prev_str, amt = stack.pop()
+                curr = prev_str + curr * amt
+            else:
+                curr += c
+        
+        return curr
